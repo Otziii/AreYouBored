ec394b6266d83d6f5fd0b0ff528ec5c73a2a7f45
Øivind Jorfald <oivind.jorfald@outlook.com>
2021-04-06 10:11:25 +0200

    First commit

---
+++ app/src/main/java/com/jorfald/areyoubored/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,18 @@
+package com.jorfald.areyoubored.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+
+
+    fun testViewModel() {
+
+    }
+}
